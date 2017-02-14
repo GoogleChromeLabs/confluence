6a109e82b679871f8b50fd8312dd09324efed261
Mark Dittmer <markdittmer@google.com>
2017-02-14 16:33:46 -0500

    Add initial code for project tests

---
+++ config/karma.conf.js
@@ -0,0 +1,101 @@
+/**
+ * @license
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+// Run all tests in Karma.
+
+const basePath = `${__dirname}/../test`;
+const deps = [];
+const entries = [
+  '../main/*.js',
+];
+const helpers = [
+  'any/**/*-helper*.js',
+  'browser/**/*-helper*.js',
+];
+const units = [
+  'any/**/*-test*.js',
+  'browser/**/*-test*.js',
+];
+const integrations = [
+  'any/**/*-integration*.js',
+  'browser/**/*-integration*.js',
+];
+const reporters = ['progress'];
+const preprocessors = entries.reduce((acc, key) => {
+  acc[key] = ['webpack'];
+  return acc;
+}, {});
+
+function configurator(config) {
+  config.set({
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath,
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+    // list of files / patterns to load in the browser
+    files: deps,
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters,
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors,
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR ||
+    // config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // start these browsers
+    // available browser launchers:
+    // https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // enable / disable watching file and executing tests whenever any file
+    // changes
+    autoWatch: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+  });
+};
+
+configurator.deps = deps;
+configurator.entries = entries;
+configurator.helpers = helpers;
+configurator.units = units;
+configurator.integrations = integrations;
+
+module.exports = configurator;
