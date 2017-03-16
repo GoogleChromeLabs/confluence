beeb837fd38d4a5d1388898e9a3fcd73b628e011
jing <taojing@google.com>
2017-03-16 14:19:15 -0400

    nit: Change 'Window' to 'Windwos'; Add helper function to create  browserAPI instances; add done() in asynch tests

--- test/any/client/api_matrix-test.es6.js
+++ test/any/client/api_matrix-test.es6.js
@@ -18,76 +18,39 @@
 
 describe('ApiMatrix', function() {
   let apiMatrix;
+  let browserAPI = function(browserName, browserVersion,
+    osName, osVersion, interfaceName, apiName) {
+      return org.chromium.apis.web.BrowserAPI.create({
+        browserName: browserName,
+        browserVersion: browserVersion,
+        osName: osName,
+        osVersion: osVersion,
+        interfaceName: interfaceName,
+        apiName: apiName,
+      });
+    };
   beforeEach(function() {
     let browserApiDao = foam.dao.EasyDAO.create({
       name: 'browserApiDao',
       of: org.chromium.apis.web.BrowserAPI,
       daoType: 'MDAO',
     });
-    browserApiDao.put(org.chromium.apis.web.BrowserAPI.create({
-      browserName: 'Chrome',
-      browserVersion: '55',
-      osName: 'Window',
-      osVersion: '10',
-      interfaceName: 'Array',
-      apiName: 'find',
-    }));
-    browserApiDao.put(org.chromium.apis.web.BrowserAPI.create({
-      browserName: 'Chrome',
-      browserVersion: '55',
-      osName: 'Window',
-      osVersion: '10',
-      interfaceName: 'Audio',
-      apiName: 'stop',
-    }));
-    browserApiDao.put(org.chromium.apis.web.BrowserAPI.create({
-      browserName: 'Edge',
-      browserVersion: '14',
-      osName: 'Window',
-      osVersion: '10',
-      interfaceName: 'Array',
-      apiName: 'find',
-    }));
-    browserApiDao.put(org.chromium.apis.web.BrowserAPI.create({
-      browserName: 'Edge',
-      browserVersion: '14',
-      osName: 'Window',
-      osVersion: '10',
-      interfaceName: 'Audio',
-      apiName: 'play',
-    }));
-    browserApiDao.put(org.chromium.apis.web.BrowserAPI.create({
-      browserName: 'Safari',
-      browserVersion: '10',
-      osName: 'OSX',
-      osVersion: '601',
-      interfaceName: 'ApplePay',
-      apiName: 'about',
-    }));
-    browserApiDao.put(org.chromium.apis.web.BrowserAPI.create({
-      browserName: 'Safari',
-      browserVersion: '10',
-      osName: 'OSX',
-      osVersion: '601',
-      interfaceName: 'Audio',
-      apiName: 'play',
-    }));
-    browserApiDao.put(org.chromium.apis.web.BrowserAPI.create({
-      browserName: 'Safari',
-      browserVersion: '10',
-      osName: 'OSX',
-      osVersion: '601',
-      interfaceName: 'Audio',
-      apiName: 'stop',
-    }));
-    browserApiDao.put(org.chromium.apis.web.BrowserAPI.create({
-      browserName: 'Safari',
-      browserVersion: '10',
-      osName: 'OSX',
-      osVersion: '601',
-      interfaceName: 'Array',
-      apiName: 'find',
-    }));
+    browserApiDao.put(browserAPI('Chrome', '55', 'Windows', '10',
+      'Array', 'find'));
+    browserApiDao.put(browserAPI('Chrome', '55', 'Windows', '10',
+      'Audio', 'stop'));
+    browserApiDao.put(browserAPI('Edge', '14', 'Windows', '10',
+      'Array', 'find'));
+    browserApiDao.put(browserAPI('Edge', '14', 'Windows', '10',
+      'Audio', 'play'));
+    browserApiDao.put(browserAPI('Safari', '10', 'OSX', '601',
+      'ApplePay', 'about'));
+    browserApiDao.put(browserAPI('Safari', '10', 'OSX', '601',
+      'Audio', 'play'));
+    browserApiDao.put(browserAPI('Safari', '10', 'OSX', '601',
+      'Audio', 'stop'));
+    browserApiDao.put(browserAPI('Safari', '10', 'OSX', '601',
+      'Array', 'find'));
     apiMatrix = org.chromium.apis.web.ApiMatrix.create({
       browserAPIs: browserApiDao,
     });
@@ -95,138 +58,147 @@ describe('ApiMatrix', function() {
 
   describe('toMatrix()', function() {
     it(`contains correct interface and API information, when all browsers
-      are selected`, function() {
+      are selected`, function(done) {
         apiMatrix.toMatrix([
-          'Chrome_55_Window_10',
-          'Edge_14_Window_10',
+          'Chrome_55_Windows_10',
+          'Edge_14_Windows_10',
           'Safari_10_OSX_601',
         ]).then((interfaceMatrix) => {
           expect(interfaceMatrix.ApplePay.about).toEqual({
             Safari_10_OSX_601: true,
           });
-          expect(interfaceMatrix.ApplePay.about.Chrome_55_Window_10)
+          expect(interfaceMatrix.ApplePay.about.Chrome_55_Windows_10)
             .toBeUndefined();
-          expect(interfaceMatrix.ApplePay.about.Edge_14_Window_10)
+          expect(interfaceMatrix.ApplePay.about.Edge_14_Windows_10)
             .toBeUndefined();
           expect(interfaceMatrix.Array.find).toEqual({
-            Chrome_55_Window_10: true,
-            Edge_14_Window_10: true,
+            Chrome_55_Windows_10: true,
+            Edge_14_Windows_10: true,
             Safari_10_OSX_601: true,
           });
           expect(interfaceMatrix.Audio.play).toEqual({
-            Edge_14_Window_10: true,
+            Edge_14_Windows_10: true,
             Safari_10_OSX_601: true,
           });
           expect(interfaceMatrix.Audio.stop).toEqual({
-            Chrome_55_Window_10: true,
+            Chrome_55_Windows_10: true,
             Safari_10_OSX_601: true,
           });
         });
+        done();
       });
     it(`contains correct interface and API information, when part of
-      keys are selected.`, function() {
+      keys are selected.`, function(done) {
         apiMatrix.toMatrix([
-          'Chrome_55_Window_10',
-          'Edge_14_Window_10',
+          'Chrome_55_Windows_10',
+          'Edge_14_Windows_10',
         ]).then((interfaceMatrix) => {
           expect(interfaceMatrix.Array.find).toEqual({
-            Chrome_55_Window_10: true,
-            Edge_14_Window_10: true,
+            Chrome_55_Windows_10: true,
+            Edge_14_Windows_10: true,
           });
           expect(interfaceMatrix.Audio.play).toEqual({
-            Edge_14_Window_10: true,
+            Edge_14_Windows_10: true,
           });
           expect(interfaceMatrix.Audio.stop).toEqual({
-            Chrome_55_Window_10: true,
+            Chrome_55_Windows_10: true,
           });
+          done();
         });
       });
     it('returns empty object when given array of browser keys is empty.',
-      function() {
+      function(done) {
         apiMatrix.toMatrix([]).then((interfaceMatrix) => {
           expect(interfaceMatrix).toEqual({});
+          done();
         });
     });
     it('produces correct result when unknown browser key is given.',
-      function() {
+      function(done) {
         apiMatrix.toMatrix([
-          'Chrome_55_Window_10',
-          'IE_10_Window_8',
+          'Chrome_55_Windows_10',
+          'IE_10_Windows_8',
         ]).then((interfaceMatrix) => {
           expect(interfaceMatrix).toEqual({
             Array: {
               find: {
-                Chrome_55_Window_10: true,
+                Chrome_55_Windows_10: true,
               },
             },
             Audio: {
               stop: {
-                Chrome_55_Window_10: true,
+                Chrome_55_Windows_10: true,
               },
             },
           });
+          done();
         });
     });
   });
 
   describe('toCSV()', function() {
     it(`produces correct csv string when all browsers are selected`,
-    function() {
+    function(done) {
       apiMatrix.toCSV([
-        'Chrome_55_Window_10',
-        'Edge_14_Window_10',
+        'Chrome_55_Windows_10',
+        'Edge_14_Windows_10',
         'Safari_10_OSX_601',
       ]).then((csvStr) => {
         expect(csvStr.split('\n').sort()).toEqual(
-          ('Interface,API,Chrome_55_Window_10,Edge_14_Window_10,' +
+          ('Interface,API,Chrome_55_Windows_10,Edge_14_Windows_10,' +
           'Safari_10_OSX_601\n' + 'Array,find,true,true,true\n' +
           'Audio,play,false,true,true\n' + 'Audio,stop,true,false,true\n' +
           'ApplePay,about,false,false,true\n').split('\n').sort()
         );
+        done();
       });
     });
     it(`produces correct csv string when part of browsers are selected`,
-    function() {
+    function(done) {
       apiMatrix.toCSV([
-        'Chrome_55_Window_10',
-        'Edge_14_Window_10',
+        'Chrome_55_Windows_10',
+        'Edge_14_Windows_10',
       ]).then((csvStr) => {
         expect(csvStr.split('\n').sort()).toEqual(
-          ('Interface,API,Chrome_55_Window_10,Edge_14_Window_10\n' +
+          ('Interface,API,Chrome_55_Windows_10,Edge_14_Windows_10\n' +
           'Array,find,true,true\n' + 'Audio,stop,true,false\n' +
           'Audio,play,false,true\n').split('\n').sort()
         );
+        done();
       });
     });
     it('returns empty csv when given array of browser keys is empty.',
-      function() {
+      function(done) {
         apiMatrix.toCSV([]).then((csvStr) => {
           expect(csvStr).toEqual('Interface,API\n');
+          done();
         });
     });
     it('lists  all false for a unknown browser key.',
-      function() {
+      function(done) {
         apiMatrix.toCSV([
-          'Chrome_55_Window_10',
-          'IE_10_Window_8',
+          'Chrome_55_Windows_10',
+          'IE_10_Windows_8',
         ]).then((csvStr) => {
           expect(csvStr.split('\n').sort()).toEqual(
-            ('Interface,API,Chrome_55_Window_10,IE_10_Window_8\n' +
+            ('Interface,API,Chrome_55_Windows_10,IE_10_Windows_8\n' +
             'Array,find,true,false\n' +
             'Audio,stop,true,false\n').split('\n').sort());
+          done();
         });
     });
     it('has a table header with the same order of given browser keys',
-      function() {
+      function(done) {
         apiMatrix.toCSV([
-          'Edge_14_Window_10',
+          'Edge_14_Windows_10',
           'Safari_10_OSX_601',
-          'Chrome_55_Window_10',
+          'Chrome_55_Windows_10',
         ]).then((csvStr) => {
           expect(csvStr.split('\n')[0]).toEqual(
-            'Interface,API,Edge_14_Window_10,' +
-            'Safari_10_OSX_601,Chrome_55_Window_10'
+            'Interface,API,Edge_14_Windows_10,' +
+            'Safari_10_OSX_601,Chrome_55_Windows_10'
           );
+          done();
         });
       });
   });
