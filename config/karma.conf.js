cb2b91161a60d09dbaef5903a483f06b2b90fcb1
jing <taojing@google.com>
2017-03-17 11:02:21 -0400

    Change noActivityTimeout to be 150s.

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -97,7 +97,7 @@ function configurator(config) {
     // how many browser should be started simultaneous
     concurrency: Infinity,
 
-    browserNoActivityTimeout: 60000,
+    browserNoActivityTimeout: 150000,
   });
 };
 
