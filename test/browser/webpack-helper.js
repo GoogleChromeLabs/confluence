09125680233fdbdcd54c5eebd81e9337b74f052b
jing <taojing@google.com>
2017-03-31 15:28:47 -0400

    Refactor foam.relationship to a separate file. Change relationship forward name to plural form.

--- test/browser/webpack-helper.js
+++ test/browser/webpack-helper.js
@@ -13,17 +13,9 @@
 require('../../lib/web_catalog/api_extractor.es6.js');
 require('../../lib/web_apis/browser.es6.js');
 require('../../lib/web_apis/web_interface.es6.js');
-
-foam.RELATIONSHIP({
-  sourceModel: 'org.chromium.apis.web.Browser',
-  targetModel: 'org.chromium.apis.web.WebInterface',
-  forwardName: 'interface',
-  inverseName: 'browser',
-  cardinality: '*:*',
-});
-
-require('../../lib/web_apis/api_importer.es6');
-require('../../lib/client/api_matrix.es6');
+require('../../lib/web_apis/browser_interface_relationship.es6.js');
+require('../../lib/web_apis/api_importer.es6.js');
+require('../../lib/client/api_matrix.es6.js');
 
 // Require external libraries.
 let libs = {
