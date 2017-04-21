3f5e2c1c3d991e49547b39b2b24f632e73187794
Mark Dittmer <mark.s.dittmer@gmail.com>
2017-04-21 08:36:45 -0400

    files-helper: Consolidate browser-only and node-only code

--- test/any/files-helper.js
+++ test/any/files-helper.js
@@ -7,32 +7,37 @@
 //
 // TODO(markdittmer): Move to FOAM classloader for this.
 
+function requireBrowserCode() {
+  // Angular-related (browser-only) code.
+  //
+  // TODO(markittmer): Fix: Alphabetical order exception: main/app defines
+  // angular module used by other browser-only code below.
+  require('../../main/app.es6.js');
+
+  require('../../lib/chart/aggressive_removal_chart.es6.js');
+  require('../../lib/chart/api_velocity_chart.es6.js');
+  require('../../lib/chart/failure_to_ship_chart.es6.js');
+  require('../../lib/chart/time_series_chart.es6.js');
+  require('../../lib/chart/vendor_specific_chart.es6.js');
+  require('../../lib/client/api_confluence.es6.js');
+  require('../../lib/client/api_service.es6.js');
+  require('../../lib/component/api_analytics.es6.js');
+  require('../../lib/component/catalog_table.es6.js');
+  require('../../lib/controller/api_catalog.es6.js');
+  require('../../lib/controller/api_confluence.es6.js');
+}
+
+function requireNodeCode() {
+  // Code invoking Node JS APIs.
+  require('../../lib/datastore/datastore_importer.es6.js');
+}
+
 beforeAll(function() {
-  // Use of foam.isServer = <running in Node JS?>:
-  // - Do not load Angular-related in Node JS (foam.isServer = true).
-  // - Do not load import-from-filesystem in browser.
-
-  if (!foam.isServer) {
-    // TODO(markittmer): Fix: Alphabetical order exception: main/app defines
-    // angular module used by other code.
-    require('../../main/app.es6.js');
-
-    require('../../lib/chart/aggressive_removal_chart.es6.js');
-    require('../../lib/chart/api_velocity_chart.es6.js');
-    require('../../lib/chart/failure_to_ship_chart.es6.js');
-    require('../../lib/chart/time_series_chart.es6.js');
-    require('../../lib/chart/vendor_specific_chart.es6.js');
-    require('../../lib/client/api_confluence.es6.js');
-  }
+  if (foam.isServer) requireNodeCode();
+  else requireBrowserCode();
 
   // Client-side, but used in server integration tests.
   require('../../lib/client/api_matrix.es6.js');
-
-  if (!foam.isServer) {
-    require('../../lib/client/api_service.es6.js');
-    require('../../lib/component/api_analytics.es6.js');
-    require('../../lib/component/catalog_table.es6.js');
-  }
 
   require('../../lib/confluence/aggressive_removal_data.es6.js');
   require('../../lib/confluence/aggressive_removal.es6.js');
@@ -43,16 +48,6 @@ beforeAll(function() {
   require('../../lib/confluence/metric_computer.es6.js');
   require('../../lib/confluence/vendor_specific_data.es6.js');
   require('../../lib/confluence/vendor_specific.es6.js');
-
-  if (!foam.isServer) {
-    require('../../lib/controller/api_catalog.es6.js');
-    require('../../lib/controller/api_confluence.es6.js');
-  }
-
-  if (foam.isServer) {
-    require('../../lib/datastore/datastore_importer.es6.js');
-  }
-
   require('../../lib/datastore/rate_limited_DAO.es6.js');
   require('../../lib/datastore/rate_limiter.es6.js');
   require('../../lib/web_apis/api_importer.es6.js');
