b77a230f63eafb6f21f1032905110ba0b307ce13
Mark Dittmer <mdittmer@users.noreply.github.com>
2017-06-15 16:13:40 -0400

    Switch to using Datastore in serve.js (#73)
    - Add required composite index to index.yaml
    - nit: Singularize "aggresiveRemovalMetric" in Angular scope (for consistency)
    - Unify Angular chart classes (underlying data model was already unified)
    - Do not auto-validate BrowserMetricData: May be created empty by DatastoreDAO
    - Delete "api analytics" that contained duplicate info in analysis chart

--- main/serve.js
+++ main/serve.js
@@ -3,76 +3,53 @@
 // found in the LICENSE file.
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 global.FOAM_FLAGS = {gcloud: true};
 require('foam2');
-require('../lib/web_apis/api_importer.es6');
-require('../lib/web_catalog/api_extractor.es6');
-require('../lib/confluence/metric_computer.es6.js');
-require('../lib/confluence/api_velocity.es6.js');
-require('../lib/confluence/failure_to_ship.es6.js');
-require('../lib/confluence/browser_specific.es6.js');
 require('../lib/confluence/aggressive_removal.es6.js');
+require('../lib/confluence/api_velocity.es6.js');
+require('../lib/confluence/browser_specific.es6.js');
+require('../lib/confluence/failure_to_ship.es6.js');
+require('../lib/datastore/datastore_container.es6.js');
+require('../lib/web_apis/release.es6.js');
+require('../lib/web_apis/release_interface_relationship.es6.js');
+require('../lib/web_apis/web_interface.es6.js');
 
-let server = foam.net.node.Server.create({
-  port: 9000,
+let server = foam.lookup('foam.net.node.Server').create({
+  port: 9010,
 });
 
-// Use object graph data from ../data/og directory for test
-// before cloudstore DAO is done.
-const OG_DATA_PATH = `${__dirname}/../data/og`;
-let extractor = org.chromium.apis.web.ApiExtractor.create();
-let apiImporter = org.chromium.apis.web.ApiImporter.create();
-const objectGraph = require('object-graph-js').ObjectGraph;
-const fs = require('fs');
-let ogFiles = fs.readdirSync(OG_DATA_PATH);
-
-for (let i = 0; i < ogFiles.length; i += 1) {
-  console.log(`read object graph file ${ogFiles[i]}`);
-  let filePath = `${OG_DATA_PATH}/${ogFiles[i]}`;
-  let stat = fs.statSync(filePath);
-  if (stat.isFile()) {
-    let releaseInfo = ogFiles[i].slice(0, -5).split('_');
-    // Object graph files starts with "window_".
-    if (releaseInfo[0] !== 'window') continue;
-    apiImporter.import(releaseInfo[1], releaseInfo[2], releaseInfo[3],
-      releaseInfo[4], extractor.extractWebCatalog(objectGraph
-      .fromJSON(JSON.parse(fs.readFileSync(filePath)))));
-  }
-}
-
-let apiVelocity = org.chromium.apis.web.ApiVelocity.create({
-  releaseDAO: apiImporter.releaseDAO,
-  webInterfaceDAO: apiImporter.webInterfaceDAO,
-  releaseWebInterfaceJunctionDAO: apiImporter.releaseWebInterfaceJunctionDAO,
-});
-
-let failureToShip = org.chromium.apis.web.FailureToShip.create({
-  releaseDAO: apiImporter.releaseDAO,
-  webInterfaceDAO: apiImporter.webInterfaceDAO,
-  releaseWebInterfaceJunctionDAO: apiImporter.releaseWebInterfaceJunctionDAO,
-});
-
-let browserSpecific = org.chromium.apis.web.BrowserSpecific.create({
-  releaseDAO: apiImporter.releaseDAO,
-  webInterfaceDAO: apiImporter.webInterfaceDAO,
-  releaseWebInterfaceJunctionDAO: apiImporter.releaseWebInterfaceJunctionDAO,
-});
-
-let aggressiveRemoval = org.chromium.apis.web.AggressiveRemoval.create({
-  releaseDAO: apiImporter.releaseDAO,
-  webInterfaceDAO: apiImporter.webInterfaceDAO,
-  releaseWebInterfaceJunctionDAO: apiImporter.releaseWebInterfaceJunctionDAO,
-});
+const credentials = JSON.parse(fs.readFileSync(
+    path.resolve(__dirname, '../.local/credentials.json')));
+const logger = foam.lookup('foam.nanos.log.ConsoleLogger').create();
+const ctx = foam.lookup('org.chromium.apis.web.DatastoreContainer').create({
+  gcloudAuthEmail: credentials.client_email,
+  gcloudAuthPrivateKey: credentials.private_key,
+  gcloudProjectId: credentials.project_id,
+  logger: logger,
+}).ctx;
 
 server.exportFile('/', `${__dirname}/../static/index.html`);
 
-server.exportDAO(apiImporter.releaseDAO, '/releases');
-server.exportDAO(apiImporter.webInterfaceDAO, '/web-interfaces');
-server.exportDAO(apiImporter.releaseWebInterfaceJunctionDAO, '/release-apis');
-server.exportDAO(apiVelocity.apiVelocityDAO, '/api-velocity');
-server.exportDAO(failureToShip.failureToShipDAO, '/failure-to-ship');
-server.exportDAO(browserSpecific.browserSpecificDAO, '/browser-specific');
-server.exportDAO(aggressiveRemoval.aggressiveRemovalDAO, '/aggressive-removal');
+server.exportDAO(ctx.releaseDAO, '/releases');
+server.exportDAO(ctx.webInterfaceDAO, '/web-interfaces');
+server.exportDAO(ctx.releaseWebInterfaceJunctionDAO, '/release-apis');
+
+server.exportDAO(ctx.apiVelocityDAO, '/api-velocity');
+
+const E = foam.lookup('foam.mlang.ExpressionsSingleton').create();
+const EQ = E.EQ.bind(E);
+const Type = foam.lookup('org.chromium.apis.web.BrowserMetricDataType');
+const TYPE = foam.lookup('org.chromium.apis.web.BrowserMetricData').TYPE;
+
+server.exportDAO(ctx.browserMetricsDAO.where(EQ(TYPE, Type.FAILURE_TO_SHIP)),
+                 '/failure-to-ship');
+server.exportDAO(ctx.browserMetricsDAO.where(EQ(TYPE, Type.BROWSER_SPECIFIC)),
+                 '/browser-specific');
+server.exportDAO(ctx.browserMetricsDAO.where(EQ(TYPE, Type.AGGRESSIVE_REMOVAL)),
+                 '/aggressive-removal');
 
 server.exportDirectory('/images', `${__dirname}/../static/images`);
 
