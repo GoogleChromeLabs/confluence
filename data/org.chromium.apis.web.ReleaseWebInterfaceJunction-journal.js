74e30a4a209917b95f3b004f4088adf52b46cd76
Mark Dittmer <markdittmer@google.com>
2017-09-25 11:43:13 -0400

    Store unversioned data extracted from ObjectGraphData
    This is towards a move for multi-stage data update:
    
    ObjectGraph => Unversioned Journaled => Datastore + Versioned Journaled

---
+++ data/org.chromium.apis.web.ReleaseWebInterfaceJunction-journal.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8cb6dd591fbec183549e0127ea8a531dced1410514c0bf5ced77e53043165a64
+size 52580147
---
+++ data/org.chromium.apis.web.WebInterface-journal.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7f0a70401b94700ff1e99e523287dafbbd4f85a6f72aa96b76b04d2901f2e082
+size 39780300
