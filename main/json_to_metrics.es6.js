087ec29ee681fa2f2224c19d68338d473470f128
Mark Dittmer <markdittmer@google.com>
2017-10-30 16:09:55 -0400

    WIP: Got MetricComputers working; half-done setting up Runner<=>Service bridge to work for ApiVelocity too

--- main/json_to_metrics.es6.js
+++ main/json_to_metrics.es6.js
@@ -8,8 +8,8 @@ const path = require('path');
 global.FOAM_FLAGS = {gcloud: true};
 require('foam2');
 
-require('../lib/confluence/api_velocity.es6.js');
 require('../lib/confluence/api_velocity_data.es6.js');
+require('../lib/confluence/api_velocity_runner.es6.js');
 require('../lib/confluence/browser_metric_data.es6.js');
 require('../lib/confluence/metric_computer_runner.es6.js');
 require('../lib/dao_container.es6.js');
@@ -23,33 +23,21 @@ const pkg = org.chromium.apis.web;
 // Setup logging, containing context, runner
 //
 
-const logger = foam.log.ConsoleLogger.create();
+// Compute metrics for non-mobile releases.
+const E = foam.mlang.ExpressionsSingleton.create();
+const logger = foam.log.ConsoleLogger.create(null, foam.createSubContext({
+  releasePredicate: E.EQ(pkg.Release.IS_MOBILE, false),
+}));
 
 let container = pkg.DAOContainer.create(null, logger);
 
-// Runner + ApiVelocity input: Releases that are grouped and sent to computer
-// services.
 container.releaseDAO = pkg.LocalJsonDAO.create({
   of: pkg.Release,
   path: `${__dirname}/../data/json/${pkg.Release.id}.json`
 }, container);
-
-// ApiVelocity input: Locally computes API count deltas.
-container.webInterfaceDAO = pkg.LocalJsonDAO.create({
-  of: pkg.WebInterface,
-  path: `${__dirname}/../data/json/${pkg.WebInterface.id}.json`
-}, container);
-container.releaseWebInterfaceJunctionDAO = pkg.LocalJsonDAO.create({
-  of: pkg.ReleaseWebInterfaceJunction,
-  path: `${__dirname}/../data/json/${pkg.ReleaseWebInterfaceJunction.id}.json`
-}, container);
-
-// Runner output: Browser metrics.
 container.browserMetricsDAO = foam.dao.MDAO.create({
   of: pkg.BrowserMetricData,
 }, container);
-
-// ApiVelocity output: API velocity data.
 container.apiVelocityDAO = foam.dao.MDAO.create({
   of: pkg.ApiVelocityData,
 }, container);
@@ -60,10 +48,7 @@ const runner = pkg.MetricComputerRunner.create(null, container);
 // Compute data, then store it in data/json/{class}.json
 //
 
-Promise.all([
-  runner.run(),
-  pkg.ApiVelocity.create(null, container),
-]).then(() => {
+runner.run().then(() => {
   const outputter = foam.json.Outputter.create({
     pretty: false,
     formatDatesAsNumbers: true,
@@ -91,7 +76,6 @@ Promise.all([
     });
   }
 
-  const E = foam.mlang.ExpressionsSingleton.create();
   return Promise.all([
     container.browserMetricsDAO
         .orderBy(E.THEN_BY(pkg.BrowserMetricData.TYPE,
