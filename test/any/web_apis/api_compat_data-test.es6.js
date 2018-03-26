328106e3126e3d25a5b49d10b72a5c6018dc74cc
Mark Dittmer <mark.s.dittmer@gmail.com>
2018-03-26 10:33:57 -0400

    Port compat data model generator from mdittmer/mdn-confulence
    Generates a custom class for compat data over a specific set of releases.

--- test/any/web_apis/api_compat_data-test.es6.js
+++ test/any/web_apis/api_compat_data-test.es6.js
@@ -56,3 +56,94 @@ describe('AbstractApiCompatData', () => {
         apiName: 'a',
       }));
 });
+
+fdescribe('CompatClassGenerator', () => {
+  let CompatProperty;
+  let Release;
+  let generator;
+  beforeEach(() => {
+    CompatProperty = org.chromium.apis.web.CompatProperty;
+    Release = org.chromium.apis.web.Release;
+    generator = org.chromium.apis.web.CompatClassGenerator.create();
+  });
+
+  it('should register a class', () => {
+    generator.generateClass(
+        generator.generateSpec('org.chromium.apis.web', 'CompatClass', []));
+    expect(foam.lookup('org.chromium.apis.web.CompatClass')).toBeDefined();
+  });
+
+  it('should accept a model', () => {
+    const model = foam.json.parse(
+      generator.generateSpec('org.chromium.apis.web', 'CompatClass', []));
+    expect(foam.core.Model.isInstance(model)).toBe(true);
+    generator.generateClass(model);
+    expect(foam.lookup('org.chromium.apis.web.CompatClass')).toBeDefined();
+  });
+
+  it('should create a property for each release',  () => {
+    const release0 = Release.create({
+      browserName: 'Alpha',
+      browserVersion:'1.2.3',
+      osName: 'Zulu',
+      osVersion: '9.8.7',
+    });
+    const release1 = Release.create({
+      browserName: 'Beta',
+      browserVersion:'3.2.1',
+      osName: 'Yankee',
+      osVersion: '7.8.9',
+    });
+    const Cls = generator.generateClass(
+        generator.generateSpec('org.chromium.apis.web', 'CompatClass', [
+          release0,
+          release1,
+        ]));
+    const props = Cls.getAxiomsByClass(CompatProperty);
+    expect(props.length).toBe(2);
+    expect(foam.util.equals(props[0].release, release0)).toBe(true);
+    expect(foam.util.equals(props[1].release, release1)).toBe(true);
+  });
+
+  it('should error out validating with duplicate relases', () => {
+    expect(() => generator.generateClass(
+        generator.generateSpec('org.chromium.apis.web', 'CompatClass', [
+          Release.create({
+            browserName: 'Alpha',
+            browserVersion:'1.2.3',
+            osName: 'Zulu',
+            osVersion: '9.8.7',
+          }),
+          Release.create({
+            browserName: 'Alpha',
+            browserVersion:'1.2.3',
+            osName: 'Zulu',
+            osVersion: '9.8.7',
+          }),
+        ]))).toThrow();
+  });
+
+  it('should disambiguate similar versions', () => {
+    const release0 = Release.create({
+      browserName: 'Alpha',
+      browserVersion:'1.2.3',
+      osName: 'Zulu',
+      osVersion: '9.8.7',
+    });
+    const release1 = Release.create({
+      browserName: 'Alpha',
+      browserVersion:'1.23',
+      osName: 'Zulu',
+      osVersion: '9.8.7',
+    });
+    const Cls = generator.generateClass(
+        generator.generateSpec('org.chromium.apis.web', 'CompatClass', [
+          release0,
+          release1,
+        ]));
+    const props = Cls.getAxiomsByClass(CompatProperty);
+    expect(props.length).toBe(2);
+    expect(foam.util.equals(props[0].release, release0)).toBe(true);
+    expect(foam.util.equals(props[1].release, release1)).toBe(true);
+  });
+});
