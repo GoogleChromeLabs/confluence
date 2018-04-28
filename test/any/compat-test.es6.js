d7696ed8554d23adf1701f07a4e405cfb4ce6f6e
Mark Dittmer <mark.s.dittmer@gmail.com>
2018-04-28 15:47:31 -0400

    Test and implement class generator for compat data

---
+++ test/any/compat-test.es6.js
@@ -0,0 +1,15 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+describe('ClassGenerator', () => {
+  fit('should load compat data class from URL', done => {
+    org.chromium.apis.web.ClassGenerator.create({
+      classURL: 'https://storage.googleapis.com/web-api-confluence-data-cache/latest/json/class%3Aorg.chromium.apis.web.generated.CompatData.json',
+    }).generateClass().then(cls => {
+      expect(cls.id).toBe('org.chromium.apis.web.generated.CompatData');
+      done();
+    }, done.fail);
+  });
+})
