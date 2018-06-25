8ecf2ce726913aa1791d04807bdba7923a83cdaf
Mark Dittmer <markdittmer@google.com>
2018-06-25 10:48:10 -0400

    Refactor faking of generated.CompatData for reuse in other tests

--- test/any/confluence/api_count-test.es6.js
+++ test/any/confluence/api_count-test.es6.js
@@ -29,22 +29,7 @@ describe('ApiCount', () => {
   const init = releaseSpecs => {
     // Register custom CompatData before looking up classes and instantiating
     // instances.
-    foam.CLASS({
-      name: 'CompatData',
-      package: 'org.chromium.apis.web.test',
-      extends: 'org.chromium.apis.web.AbstractApiCompatData',
-
-      properties: releaseSpecs.map(r => {
-        return {
-          class: 'org.chromium.apis.web.CompatProperty',
-          release: r,
-          name: gen.propertyNameFromRelease(r),
-          label: gen.propertyLabelFromRelease(r),
-        };
-      }),
-    });
-    foam.register(org.chromium.apis.web.test.CompatData, 'org.chromium.apis.web.generated.CompatData');
-    CompatData = foam.lookup('org.chromium.apis.web.generated.CompatData');
+    CompatData = global.defineGeneratedCompatData(releaseSpecs);
 
     Release = foam.lookup('org.chromium.apis.web.Release');
     ApiCountData = foam.lookup('org.chromium.apis.web.ApiCountData');
---
+++ test/any/generated_compat_data-helper.es6.js
@@ -0,0 +1,26 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+global.defineGeneratedCompatData = releaseSpecs => {
+  // Register custom CompatData before looking up classes and instantiating
+  // instances.
+  foam.CLASS({
+    name: 'CompatData',
+    package: 'org.chromium.apis.web.test',
+    extends: 'org.chromium.apis.web.AbstractApiCompatData',
+
+    properties: releaseSpecs.map(r => {
+      return {
+        class: 'org.chromium.apis.web.CompatProperty',
+        release: r,
+        name: gen.propertyNameFromRelease(r),
+        label: gen.propertyLabelFromRelease(r),
+      };
+    }),
+  });
+  foam.register(org.chromium.apis.web.test.CompatData,
+      'org.chromium.apis.web.generated.CompatData');
+  return foam.lookup('org.chromium.apis.web.generated.CompatData');
+};
