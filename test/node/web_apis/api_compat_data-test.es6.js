21f31555aec58c5917588fb5ecee753d516216f2
Philip Jägenstedt <philip@foolip.org>
2019-04-26 14:26:02 +0200

    Make one test depend on the Node.JS version
    Fixes https://github.com/GoogleChromeLabs/confluence/issues/338.

--- test/node/web_apis/api_compat_data-test.es6.js
+++ test/node/web_apis/api_compat_data-test.es6.js
@@ -11,21 +11,44 @@ describe('CompatClassGenerator', () => {
     generator = org.chromium.apis.web.CompatClassGenerator.create();
   });
 
-  it('should fail console.assert() validating with duplicate relases', () => {
-    expect(() => generator.generateClass(
-        generator.generateSpec('org.chromium.apis.web', 'CompatClass', [
-          Release.create({
-            browserName: 'Alpha',
-            browserVersion:'1.2.3',
-            osName: 'Zulu',
-            osVersion: '9.8.7',
-          }),
-          Release.create({
-            browserName: 'Alpha',
-            browserVersion:'1.2.3',
-            osName: 'Zulu',
-            osVersion: '9.8.7',
-          }),
-        ]))).toThrow();
+  function generateClass(dupe) {
+    return generator.generateClass(
+      generator.generateSpec('org.chromium.apis.web', 'CompatClass', [
+        Release.create({
+          browserName: 'Alpha',
+          browserVersion:'1.2.3',
+          osName: 'Zulu',
+          osVersion: '9.8.7',
+        }),
+        Release.create({
+          browserName: dupe ? 'Alpha' : 'Beta',
+          browserVersion:'1.2.3',
+          osName: 'Zulu',
+          osVersion: '9.8.7',
+        }),
+      ]));
+  }
+
+  it('should handle multiple relases', () => {
+    const CompatData = generateClass(false);
+    const axioms = CompatData.getAxiomsByClass(org.chromium.apis.web.CompatProperty);
+    expect(axioms.length).toBe(2);
   });
+
+  // The behavior of `console.assert` changed in Node.JS 10, which is used
+  // internally in foam2. This means the expected behavior depends on the
+  // Node.JS version.
+  // See https://github.com/GoogleChromeLabs/confluence/issues/338
+  if (parseInt(process.versions.node.split('.')[0]) >= 10) {
+    it('should ignore duplicate relases', () => {
+      const CompatData = generateClass(true);
+      const axioms = CompatData.getAxiomsByClass(org.chromium.apis.web.CompatProperty);
+      expect(axioms.length).toBe(1);
+    });
+  } else {
+    it('should throw for duplicate relases', () => {
+      expect(() => generateClass(true)).toThrow();
+    });
+  }
+
 });
