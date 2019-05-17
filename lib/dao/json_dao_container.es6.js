GoogleChromeLabs/confluence

commit ce7efb823750630c0f494d1e03f54a4550bd6586

Use the URL constructor instead of url module (#409)
The URL class is available on the global object since nodejs 10:
https://nodejs.org/api/url.html#url_the_whatwg_url_api

lib/dao/json_dao_container.es6.js
@@ -108,22 +108,6 @@ foam.CLASS({
         return this.StubFactorySingleton.create();
       },
     },
-    {
-      // TODO(markdittmer): This is the second time this browser/node code smell
-      // has come up for URL parsing. Perhaps move to some kind of interop
-      // utility class.
-      class: 'Function',
-      name: 'parseURL_',
-      transient: true,
-      factory: function() {
-        if (foam.isServer) {
-          const url = require('url');
-          return url.parse.bind(url);
-        } else {
-          return (str) => new URL(str);
-        }
-      },
-    },
   ],
 
   methods: [
@@ -141,7 +125,7 @@ foam.CLASS({
       } else {
         serializableDAO = this.SerializableLocalJsonDAO.create({
           of: cls,
-          path: this.parseURL_(`${this.basename}/${cls.id}.json`).pathname,
+          path: new URL(`${this.basename}/${cls.id}.json`).pathname,
         }, ctx);
       }
       // Set propertyIndexGroups after other properties; its validation

lib/compat.es6.js
@@ -25,19 +25,6 @@ foam.CLASS({
       required: true,
     },
     {
-      class: 'Function',
-      name: 'parseURL_',
-      transient: true,
-      factory: function() {
-        if (foam.isServer) {
-          const url = require('url');
-          return url.parse.bind(url);
-        } else {
-          return (str) => new URL(str);
-        }
-      },
-    },
-    {
       class: 'FObjectProperty',
       of: 'foam.json.Parser',
       name: 'parser',
@@ -95,7 +82,7 @@ foam.CLASS({
       code: function() {
         let url;
         try {
-          url = this.parseURL_(this.classURL);
+          url = new URL(this.classURL);
         } catch (err) {
           return Promise.reject(err);
         }

lib/dao/http_json_dao.es6.js
@@ -65,17 +65,6 @@ foam.CLASS({
       },
     },
     {
-      class: 'Function',
-      name: 'createURL',
-      documentation: 'Platform-independent URL factory function.',
-      transient: true,
-      factory: function() {
-        return foam.isServer ?
-            (str) => require('url').parse(str) :
-            (str) => new URL(str);
-      },
-    },
-    {
       name: 'promise',
       transient: true,
       factory: function() {
@@ -118,7 +107,7 @@ foam.CLASS({
       if (!foam.String.isInstance(url)) {
         throw new Error('HttpJsonDAO: URL is not a String');
       }
-      if (!this.urlIsSafe(this.createURL(url))) {
+      if (!this.urlIsSafe(new URL(url))) {
         throw new Error(`HttpJsonDAO: URL is not safe: ${url}`);
       }
     },

main/relational_to_compat.es6.js
@@ -5,7 +5,6 @@
 
 const fs = require('fs');
 const process = require('process');
-const url = require('url');
 
 require('foam2');
 
@@ -43,7 +42,7 @@ if (process.argv.length !== 3) {
   process.exit(1);
 }
 
-const dataUrl = url.parse(process.argv[2]);
+const dataUrl = new URL(process.argv[2]);
 if (dataUrl.protocol !== 'file:' && dataUrl.protocol !== 'https:') {
   console.error('BaseConfluenceDataURL parameter must be file: or https: URL');
 }
@@ -52,7 +51,7 @@ const container = pkg.JsonDAOContainer.create({
   mode: dataUrl.protocol === 'file:' ? pkg.DataSource.LOCAL :
       pkg.DataSource.HTTP,
   basename: dataUrl.protocol === 'file:' ? dataUrl.pathname :
-      url.format(dataUrl),
+      dataUrl.toString(),
 }, logger);
 
 logger.info('Gathering Confluence data from JSON');

main/relational_to_grid.es6.js
@@ -5,7 +5,6 @@
 
 const fs = require('fs');
 const process = require('process');
-const url = require('url');
 
 require('foam2');
 
@@ -37,7 +36,7 @@ if (process.argv.length !== 3) {
   process.exit(1);
 }
 
-const dataUrl = url.parse(process.argv[2]);
+const dataUrl = new URL(process.argv[2]);
 if (dataUrl.protocol !== 'file:' && dataUrl.protocol !== 'https:') {
   console.error('BaseConfluenceDataURL parameter must be file: or https: URL');
 }
@@ -45,7 +44,7 @@ if (dataUrl.protocol !== 'file:' && dataUrl.protocol !== 'https:') {
 const container = pkg.JsonDAOContainer.create({
   mode: dataUrl.protocol === 'file:' ? pkg.DataSource.LOCAL :
       pkg.DataSource.HTTP,
-  basename: dataUrl.protocol === 'file:' ? dataUrl.pathname : url.format(dataUrl),
+  basename: dataUrl.protocol === 'file:' ? dataUrl.pathname : dataUrl.toString(),
 }, logger);
 
 logger.info('Gathering Confluence data from JSON');

commit 02b13f35454ae63813d4446af1807dacb7e8d3a9

Don't use URL constructor for protocol-less file system paths
This regressed with https://github.com/GoogleChromeLabs/confluence/pull/409.

The code path isn't tested, but is used by local_og_to_confluence.sh.

The problem is that `require('url').parse('/a/b/c.json')` returns a
URL with `protocol` null and pathame '/a/b/c.json', while the URL
constructor throws since it's an invalid URL.

Fixing this by prepending `file:` wouldn't even normalize the
pathname, so just don't use the URL constructor.

lib/dao/json_dao_container.es6.js
@@ -125,7 +125,7 @@ foam.CLASS({
       } else {
         serializableDAO = this.SerializableLocalJsonDAO.create({
           of: cls,
-          path: new URL(`${this.basename}/${cls.id}.json`).pathname,
+          path: `${this.basename}/${cls.id}.json`,
         }, ctx);
       }
       // Set propertyIndexGroups after other properties; its validation