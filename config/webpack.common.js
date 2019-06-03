3a7e543248e4dfc4dcd8ca0e77ef54293ee478da
Philip Jägenstedt <philip@foolip.org>
2019-06-03 12:21:43 +0200

    Adapt to https://github.com/johnagan/clean-webpack-plugin/pull/138

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -5,7 +5,7 @@
 
 const path = require('path');
 
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 const C = require('./webpack.constants.js');
 
